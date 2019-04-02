ff79d9e2d6a19d09f8ee7a4182ec8c905ffd9086
mkisantal <kisantal.mate@gmail.com>
2019-04-02 17:11:51 +0200

    adding activation to loss, removing inplace ops

--- backboned_unet/utils.py
+++ backboned_unet/utils.py
@@ -109,14 +109,14 @@ def dice_score(input, target, classes, ignore_index=-100):
     valid = valid.float()
 
     # converting to onehot image with class channels
-    onehot_target = torch.LongTensor(target.shape[0], classes, target.shape[-2], target.shape[-1]).zero_().to(device)
+    onehot_target = torch.LongTensor(target.shape[0], classes, target.shape[-2], target.shape[-1]).zero_().cuda()
     onehot_target.scatter_(1, target, 1)  # write ones along "channel" dimension
     # classes_in_image = onehot_gt_tensor.sum([2, 3]) > 0
     onehot_target = onehot_target.float()
 
     # keeping the valid pixels only
-    onehot_target *= valid
-    input *= valid
+    onehot_target = onehot_target * valid
+    input = input * valid
 
     dice = 2 * (input * onehot_target).sum([2, 3]) / ((input**2).sum([2, 3]) + (onehot_target**2).sum([2, 3]))
     return dice.mean(dim=1)
@@ -126,19 +126,22 @@ class DiceLoss(torch.nn.Module):
 
     """ Dice score implemented as a nn.Module. """
 
-    def __init__(self, classes, loss_mode='negative_log', ignore_index=255):
+    def __init__(self, classes, loss_mode='negative_log', ignore_index=255, activation=None):
         super(DiceLoss, self).__init__()
         self.classes = classes
         self.ignore_index = ignore_index
         self.loss_mode = loss_mode
+        self.activation = activation
 
     def forward(self, input, target):
+        if self.activation is not None:
+            input = self.activation(input)
         score = dice_score(input, target, self.classes, self.ignore_index)
         if self.loss_mode == 'negative_log':
             eps = 1e-12
-            return -(score+eps).log()
+            return (-(score+eps).log()).mean()
         elif self.loss_mode == 'one_minus':
-            return 1 - score
+            return (1 - score).mean()
         else:
             raise ValueError('Loss mode unknown. Please use \'negative_log\' or \'one_minus\'!')
 
@@ -147,8 +150,12 @@ if __name__ == "__main__":
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     predictions = torch.softmax(torch.empty(7, 21, 224, 224).normal_(), dim=1)
+    conv = torch.nn.Conv2d(21, 21, 1, 1, 0)
     labels = (torch.empty(7, 224, 224).normal_(10, 10) % 21)
-    loss = DiceLoss(21)
-    print(loss(predictions, labels))
+    criterion = DiceLoss(21, activation=torch.nn.Softmax(dim=1))
+    loss = criterion(conv(predictions), labels)
+
+    loss.backward()
+    print(loss)
 
     print('done.')
